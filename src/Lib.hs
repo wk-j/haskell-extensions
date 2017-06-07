ba52b38e1e892e4db6c30903b637adf3173af8fb
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-06-07 18:19:11 +0700

    Initial commit

---
+++ src/Lib.hs
@@ -0,0 +1,38 @@
+
+module Lib
+    ( someFunc
+    ) where
+
+someFunc :: IO ()
+someFunc =  do 
+    let exts = [ "DatatypeContexts"
+                , "DefaultSignatures"
+                , "DeriveDataTypable"
+                , "DeriveGeneric"
+                , "DeriveFunctor"
+                , "DeriveFoldable"
+                , "DeriveTraversable"
+                , "GeneralizedNewtypeDriving"
+                , "DuplicateRecordFields"
+                , "ExistentialQuantification"
+                , "FlexibleContexts"
+                , "FlexibleInstances"
+                , "FunctionalDependencies"
+                , "GADTs"
+                , "ImplicitParams"
+                , "KindSignatures"
+                , "MultiParamTypeClasses"
+                , "NoMonomorphismRestriction"
+                , "OverlappingInstances"
+                , "Rank2Types"
+                , "RankNTypes"
+                , "ScopedTypeVariables"
+                , "TemplateHaskell"
+                , "TupleSections"
+                , "TypeFamilies"
+                , "UndecidableInstances"
+                , "ViewPatterns" ]
+
+    let append x = "{-# LANGUAGE " ++ x ++ " #-}"
+
+    mapM_ putStrLn (map append exts)
