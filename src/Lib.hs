d5806fa1936b26d102bca7869d7e289d0c85b177
Somnuk Wongkhan <somnuk.wk@gmail.com>
2017-06-08 01:12:54 +0700

    Used pipe

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,16 +4,14 @@ module Lib
     ) where
 
 import System.Process
-import Data.List
 import Data.String.Utils
 
 (|>) x y = y x
 
 someFunc :: IO ()
 someFunc =  do
-    -- ghc --supported-languages
     ps <- readProcess "ghc" [ "--supported-languages" ] ""
-    let extensions = split "\n" ps |> filter ((/=) "")
+    let languages = ps |> split "\n" |> filter ("" /=)
 
-    let append x = "{-# LANGUAGE " ++ x ++ " #-}"
-    mapM_ putStrLn (map append extensions)
+    let append lang = "{-# LANGUAGE " ++ lang ++ " #-}"
+    languages |> mapM_ (putStrLn . append)
